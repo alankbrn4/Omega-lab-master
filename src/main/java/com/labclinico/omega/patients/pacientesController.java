b28dca082ce25260a9e6ebe97b44cae04175dabe
ALAN47 <alankbrn4@gmail.com>
2024-08-09 10:44:39 -0600

    agregado de instruccion en checker units pacientes Controller

--- src/main/java/com/labclinico/omega/patients/pacientesController.java
+++ src/main/java/com/labclinico/omega/patients/pacientesController.java
@@ -1,5 +1,6 @@
 package com.labclinico.omega.patients;
 
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import java.util.List;
 public class pacientesController {
 
     private final pacientesService pacientesService;
+    
 
     @Autowired
     public pacientesController(pacientesService pacientesService) {
@@ -19,23 +21,43 @@ public class pacientesController {
 
     @GetMapping
     public List<pacientesModel> getAllPacientes() {
+        
         return pacientesService.getAllPacientes();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<pacientesModel> getPacienteById(@PathVariable Long id) {
+        //return pacientesRepository.findById(id)
+         //       .map(ResponseEntity::ok)
+         //       .orElseThrow(() -> new RuntimeException("Patient not found with id: " + id));
+                
         return pacientesService.getPacienteById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+                
     }
 
     @PostMapping
     public pacientesModel createPaciente(@RequestBody pacientesModel paciente) {
+        //return pacientesRepository.save(paciente);
         return pacientesService.savePaciente(paciente);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<pacientesModel> updatePaciente(@PathVariable Long id, @RequestBody pacientesModel paciente) {
+        /*pacientesModel paciente = pacientesRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Patient not found with id: " + id)); 
+        paciente.setIdPaciente(id);
+        paciente.setNombre(paciente.getNombre());
+        paciente.setApellido(paciente.getApellido());
+        paciente.setEdad(paciente.getEdad());
+        paciente.setGenero(paciente.getGenero());
+        paciente.setTelefono(paciente.getTelefono());
+        
+
+        return ResponseEntity.ok(pacientesRepository.save(paciente));
+*/
+        
         return pacientesService.getPacienteById(id)
                 .map(existingPaciente -> {
                     paciente.setIdPaciente(id);
